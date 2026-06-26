8bfd6ff554820809e7995b3eac84311bffaebeb9
mostoloz42 <mostoloz@student.42urduliz.com>
2026-06-26 12:35:05 +0200

    select_sort empezado

--- mikel/select_sort.c
+++ mikel/select_sort.c
@@ -6,10 +6,26 @@
 /*   By: mostoloz <mostoloz@student.42urduliz.com>  +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2026/06/25 12:31:33 by mostoloz          #+#    #+#             */
-/*   Updated: 2026/06/25 12:41:12 by mostoloz         ###   ########.fr       */
+/*   Updated: 2026/06/26 12:34:47 by mostoloz         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "push_swap.h"
 
+t_list	**select_sort(t_list **stack_a)
+{
+	t_list	**stack_b;
+	t_list	*min_node;
+	t_list	*current_node;
+	int		min;
 
+	current_node = *stack_a;
+	min = current_node->content;
+	while (current_node)
+	{
+		while (*stack_a)
+		{
+			if ()
+		}
+	}
+}
