dcc6801311b60333bfbe76631b9aff13946c65e8
Francydani <tegnomina@gmail.com>
2026-07-14 12:21:55 +0200

    main

--- main.c
+++ main.c
@@ -6,55 +6,30 @@
 /*   By: francysa <francysa@student.42urduliz.com>  +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2026/07/06 17:11:10 by francysa          #+#    #+#             */
-/*   Updated: 2026/07/14 10:30:24 by francysa         ###   ########.fr       */
+/*   Updated: 2026/07/14 12:19:28 by francysa         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "push_swap.h"
 
-int	main(int argc, char **argv)
+void	push_swap(t_stack **stack_a, t_bench *bench)
 {
-	//int		*los_numeros;
-	//int		cuenta_numeros;
-	t_stack	**stack_a;
-	//t_stack	*stack_b;
-	//t_bench	*bench;
+	t_stack	*stack_b;
 
-	//stack_b = NULL;
-	if (argc < 2)
-		return (0);
-	///bench = malloc(sizeof(t_bench));
-	//if (!bench)
-	//	return (1);
-	//ft_bzero(bench, sizeof(t_bench));
-	//los_numeros = ft_check(argc, argv, &cuenta_numeros);
-	if (!los_numeros)
-	{
-		write(2, "Error\n", 7);
-		free(bench); // Liberamos bench para evitar leaks si falla la validación
-		return (1);
-	}
-	stack_a = create_list(los_numeros, cuenta_numeros);
-	free(los_numeros);
-	if (!stack_a)
-	{
-		free(bench);
-		return (1);
-	}
+	stack_b = NULL;
 	indexacion(*stack_a);
 	select_medium(stack_a, &stack_b, bench);
-	return (0);
 }
 
-void	push_swap(int argc, char **argv)
+int	main(int argc, char **argv)
 {
 	int		*los_numeros;
 	int		cuenta_numeros;
 	t_stack	**stack_a;
-	t_stack	*stack_b;
+	//t_stack	*stack_b;
 	t_bench	*bench;
 
-	stack_b = NULL;
+	//stack_b = NULL;
 	if (argc < 2)
 		return (0);
 	bench = malloc(sizeof(t_bench));
@@ -75,7 +50,8 @@ void	push_swap(int argc, char **argv)
 		free(bench);
 		return (1);
 	}
-	indexacion(*stack_a);
-	select_medium(stack_a, &stack_b, bench);
+	push_swap(stack_a, bench);
+	/* indexacion(*stack_a);
+	select_medium(stack_a, &stack_b, bench); */
 	return (0);
 }
